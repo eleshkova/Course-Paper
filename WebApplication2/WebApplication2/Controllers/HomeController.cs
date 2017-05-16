40b9afc4d6eb76e7359b1932251676597ed457d8
Ekaterina Leshkova <ekaterinaleshkova@gmail.com>
2017-05-16 15:44:03 +0400

    new files

---
+++ WebApplication2/WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        BookContext db = new BookContext();
+        public ActionResult Index(string genre)
+        {
+           IEnumerable<Book> books = db.Books;
+            ViewBag.Books = books;
+            return View(db.Books);
+        }
+        public ActionResult Books(string genre)
+        {
+            var allbooks = db.Books.OrderBy(a => Guid.NewGuid()).Take(1);
+            if (genre != "Любой")                
+            allbooks = db.Books.Where(a => a.Genre.Contains(genre)).OrderBy(a => Guid.NewGuid()).Take(1);
+            ViewBag.Books = allbooks;
+            return View(allbooks);
+        }
+    }
+}
---
+++ WebApplication2/WebApplication2/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication2.Models
+{
+        public class Book
+        {            
+            public int Id { get; set; }            
+            public string Name { get; set; }            
+            public string Author { get; set; }
+            public string Description { get; set; }
+            public string Genre { get; set; }
+            public string PictureUrl { get; set; }
+    }    
+}
